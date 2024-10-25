530b8f92c094c7f0d3141598c6cba43317d193bf
Luis340228 <213241056@alumnos.utn.edu.mx>
2024-10-25 00:21:57 -0600

    Agregado de archivos

--- srv/tallas.php
+++ srv/tallas.php
@@ -8,26 +8,40 @@ require_once __DIR__ . "/TABLA_TALLA.php";
 
 ejecutaServicio(function () {
 
-    // Obtenemos la lista de tallas de la base de datos
-    $lista = select(pdo: Bd::pdo(), from: TALLA, orderBy: TALL_NOMBRE);
+ /*$lista = select(pdo: Bd::pdo(),  from: TALLA,  orderBy: TALL_NOMBRE);
 
-    $render = "<dl>";
-    foreach ($lista as $modelo) {
-        $encodeId = urlencode($modelo[TALL_ID]);
-        $id = htmlentities($encodeId);
-        $nombre = htmlentities($modelo[TALL_NOMBRE]);
-        $descripcion = htmlentities($modelo[TALL_DESCRIPCION]); // Aquí asumo que hay un campo TALL_DESCRIPCION
+ $render = "";
+ foreach ($lista as $modelo) {
+  $encodeId = urlencode($modelo[TALL_ID]);
+  $id = htmlentities($encodeId);
+  $nombre = htmlentities($modelo[TALL_NOMBRE]);
+  $render .=
+   "<li>
+     <p>
+      <a href='modifica.html?id=$id'>$nombre</a>
+     </p>
+    </li>";
+ }*/
 
-        // Construimos la estructura <dl> con <dt> y <dd> para cada campo
-        $render .= "
-        <dt>Nombre</dt>
-        <dd><a href='modifica.html?id=$id'>$nombre</a></dd>
+ // Obtenemos la lista de tallas de la base de datos
+ $lista = select(pdo: Bd::pdo(), from: TALLA, orderBy: TALL_NOMBRE);
 
-        <dt>Descripción</dt>
-        <dd>$descripcion</dd>";
-    }
-    $render .= "</dl>";
+ $render = "<dl>";
+ foreach ($lista as $modelo) {
+     $encodeId = urlencode($modelo[TALL_ID]);
+     $id = htmlentities($encodeId);
+     $nombre = htmlentities($modelo[TALL_NOMBRE]);
+     $descripcion = htmlentities($modelo[TALL_DESCRIPCION]); // Aquí asumo que hay un campo TALL_DESCRIPCION
 
-    // Devolvemos el HTML generado en formato JSON
-    devuelveJson(["lista" => ["innerHTML" => $render]]);
+     // Construimos la estructura <dl> con <dt> y <dd> para cada campo
+     $render .= "
+     <dt>Nombre</dt>
+     <dd><a href='modifica.html?id=$id'>$nombre</a></dd>
+
+     <dt>Descripción</dt>
+     <dd>$descripcion</dd>";
+ }
+ $render .= "</dl>";
+
+ devuelveJson(["lista" => ["innerHTML" => $render]]);
 });
