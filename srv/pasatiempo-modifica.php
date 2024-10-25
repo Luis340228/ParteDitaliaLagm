e7d53bc73c7c42d6646f5b495a6a4ffc35418f2c
Luis340228 <213241056@alumnos.utn.edu.mx>
2024-10-24 23:49:06 -0600

    Agregado de archivos

--- srv/pasatiempo-modifica.php
+++ srv/pasatiempo-modifica.php
@@ -4,6 +4,8 @@ require_once __DIR__ . "/../lib/php/ejecutaServicio.php";
 require_once __DIR__ . "/../lib/php/recuperaIdEntero.php";
 require_once __DIR__ . "/../lib/php/recuperaTexto.php";
 require_once __DIR__ . "/../lib/php/validaNombre.php";
+require_once __DIR__ . "/../lib/php/validaDescripcion.php";
+require_once __DIR__ . "/../lib/php/validaEstado.php";
 require_once __DIR__ . "/../lib/php/update.php";
 require_once __DIR__ . "/../lib/php/devuelveJson.php";
 require_once __DIR__ . "/Bd.php";
@@ -13,8 +15,12 @@ ejecutaServicio(function () {
 
  $id = recuperaIdEntero("id");
  $nombre = recuperaTexto("nombre");
+ $descripcion = recuperaTexto("desc");
+ $estado = recuperaTexto("estado");
 
  $nombre = validaNombre($nombre);
+ $descripcion = validaDescripcion($descripcion);
+ $estado = validaEstado($estado);
 
  update(
   pdo: Bd::pdo(),
@@ -22,9 +28,23 @@ ejecutaServicio(function () {
   set: [TALL_NOMBRE => $nombre],
   where: [TALL_ID => $id]
  );
+ update(
+  pdo: Bd::pdo(),
+  table: TALLA,
+  set: [TALL_DESCRIPCION => $descripcion],
+  where: [TALL_ID => $id]
+ );
+ update(
+  pdo: Bd::pdo(),
+  table: TALLA,
+  set: [TALL_ESTADO => $estado],
+  where: [TALL_ID => $id]
+ );
 
  devuelveJson([
   "id" => ["value" => $id],
   "nombre" => ["value" => $nombre],
+  "desc" => ["value" => $descripcion],
+  "estado" => ["value" => $estado]
  ]);
 });
